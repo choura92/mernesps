54db938cb2354a67e542e5bcb8b57162b8629986
choura92 <mohamed.choura021992@gmail.com>
2023-07-18 19:05:04 +0100

    commit new version

--- routes/article.route.js
+++ routes/article.route.js
@@ -1,69 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const Article = require("../models/article")
-// créer un nouvelle article
-router.post('/', async (req, res) => {
-    const { reference, designation, prix, marque, qtestock, imageart, scategorieID, ref } = req.body;
-    const newArticle = new Article({
-        reference:reference,
-        designation:designation,
-        prix:prix,
-        marque:marque,
-        qtestock:qtestock,
-        imageart:imageart,
-        scategorieID:scategorieID,
-        ref:ref
-    })
-    try {
-        await newArticle.save();
-        res.status(200).json(newArticle);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-});
-// afficher la liste des articles. 
-router.get('/', async (req, res) => {
-    try {
-        const cat = await Article.find({}, null, { sort: { '_id': -1 } }).populate("scategorieID").exec();
+const Article=require("../models/article")
 
-        res.status(200).json(cat);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-});
-// chercher une article  
-router.get('/:articleId', async (req, res) => {
+// afficher la liste des articles.
+router.get('/', async (req, res )=> {
     try {
-        const cat = await Article.findById(req.params.articleId);
+        const articles = await Article.find({}, null, {sort: {'_id': -1}}).populate("scategorieID").exec();
+                
+        res.status(200).json(articles);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 
-        res.status(200).json(cat);
+});
+// créer un nouvel article
+router.post('/', async (req, res) =>  {
+    
+    const nouvarticle = new Article(req.body)
+
+    try {
+        await nouvarticle.save();
+
+        res.status(200).json(nouvarticle );
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+
+
+});
+// chercher un article
+router.get('/:articleId',async(req, res)=>{
+    try {
+        const art = await Article.findById(req.params.articleId);
+        
+        res.status(200).json(art);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 });
-// Supprimer une article 
-router.delete('/:articleId', async (req, res) => {
-    const id = req.params.articleId;
+// modifier un article
+
+
+router.put('/:articleId', async (req, res)=> {
+   try {
+    const art = await Article.findByIdAndUpdate(
+        req.params.articleId,
+        { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(art);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+});
+// Supprimer un article
+router.delete('/:articleId', async (req, res)=> {
+    const  id  = req.params.articleId;
     await Article.findByIdAndDelete(id);
+
     res.json({ message: "article deleted successfully." });
 
 });
-// modifier une article 
-router.put('/:articleId', async (req, res) => {
-
-    try {
-        const cat1 = await Article.findByIdAndUpdate(req.params.articleId, 
-            { 
-                $set: req.body 
-            },
-             { 
-                new: true 
-            });
-              res.status(200).json(cat1);
-
-    } catch (error) 
-    { 
-        res.status(404).json({ message: error.message }); 
-    }
-});
-    module.exports = router
+module.exports = router;
